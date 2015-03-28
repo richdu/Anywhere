1dc75891f6aab043a2b992581e114895fcb0bb3a
Richard Du <richarddu1226@gmail.com>
2015-03-28 06:06:32 -0400

    Imports

--- app/src/main/java/com/hackwestern/anywhere/MainActivity.java
+++ app/src/main/java/com/hackwestern/anywhere/MainActivity.java
@@ -1,11 +1,26 @@
 package com.hackwestern.anywhere;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.ChannelListener;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     //Whoa there dam
     @Override
     protected void onCreate(Bundle savedInstanceState) {
