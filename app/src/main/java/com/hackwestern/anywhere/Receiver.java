9a423cc130eb637a3aee4143ee6324dc64272fcd
Richard Du <richarddu1226@gmail.com>
2015-03-28 06:27:48 -0400

    stuff

--- app/src/main/java/com/hackwestern/anywhere/Receiver.java
+++ app/src/main/java/com/hackwestern/anywhere/Receiver.java
@@ -29,9 +29,9 @@ public class Receiver extends BroadcastReceiver{
             // the Activity.
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                //activity.setIsWifiP2pEnabled(true);
+                activity.setIsWifiP2pEnabled(true);
             } else {
-               // activity.setIsWifiP2pEnabled(false);
+                activity.setIsWifiP2pEnabled(false);
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
 
